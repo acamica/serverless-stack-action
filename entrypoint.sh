b3c06edb855e6ab8c7911a81eef4604aa19bb46e
Leandro <leandrotorres73@gmail.com>
2019-11-05 14:33:48 -0300

    new entry point

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,27 +2,27 @@
 
 set -e
 
-if [ -z "${{inputs.AWS_S3_BUCKET}}" ]; then
+if [ -z "$AWS_S3_BUCKET" ]; then
   echo "AWS_S3_BUCKET is not set. Quitting."
   exit 1
 fi
 
-if [ -z "${{inputs.AWS_ACCESS_KEY_ID}}" ]; then
+if [ -z "$AWS_ACCESS_KEY_ID" ]; then
   echo "AWS_ACCESS_KEY_ID is not set. Quitting."
   exit 1
 fi
 
-if [ -z "${{inputs.AWS_SECRET_ACCESS_KEY}}" ]; then
+if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
   echo "AWS_SECRET_ACCESS_KEY is not set. Quitting."
   exit 1
 fi
 
-if [ -z "${{inputs.AWS_REGION}}" ]; then
+if [ -z "$AWS_REGION" ]; then
   echo "AWS_REGION is not set. Quitting."
   exit 1
 fi
 
-if [ -z "${{inputs.STAGE}}" ]; then
+if [ -z "$STAGE" ]; then
   echo "STAGE is not set. Quitting."
   exit 1
 fi
@@ -31,13 +31,13 @@ fi
 # conflicts with other actions.
 # https://github.com/jakejarvis/s3-sync-action/issues/1
 aws configure --profile s3-download-action <<-EOF > /dev/null 2>&1
-${{inputs.AWS_ACCESS_KEY_ID}}
-${{inputs.AWS_SECRET_ACCESS_KEY}}
-${{inputs.AWS_REGION}}
+${AWS_ACCESS_KEY_ID}
+${AWS_SECRET_ACCESS_KEY}
+${AWS_REGION}
 text
 EOF
 
-for file in ${{ inputs.FILES }}
+for file in $FILES
 do
   if [[ $file == "serverless/"* ]]; then
     # get file name
@@ -46,12 +46,12 @@ do
     hash=$(<$file)
     # Use our dedicated profile and suppress verbose messages.
     # All other flags are optional via `args:` directive.
-    sh -c "aws s3 cp s3://${{inputs.AWS_S3_BUCKET}}/$hash.yml"
+    sh -c "aws s3 cp s3://${AWS_S3_BUCKET}/$hash.yml"
 
     sh -c "aws cloudformation deploy --template-file ./$hash \
-        --stack-name $filename-${{inputs.STAGE}} \
+        --stack-name $filename-${STAGE} \
         --capabilities CAPABILITY_NAMED_IAM \
-        --parameter-overrides Stage=${{inputs.STAGE}}"
+        --parameter-overrides Stage=${STAGE}"
   fi
 done
 
